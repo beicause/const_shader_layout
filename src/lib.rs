dad89313281f9c7321819268b5daff6321dfe98d
Luo Zhiaho <luo_zhihao@outlook.com>
2026-07-18 10:15:58 +0800

    Fix 16-bit vectors alignment

--- src/lib.rs
+++ src/lib.rs
@@ -47,15 +47,18 @@ impl_shader_layout_raw!(
 );
 
 // Vector
-impl_shader_layout_raw!(
-    I16Vec2, U16Vec2, I16Vec4, U16Vec4, IVec2, UVec2, Vec2, IVec4, UVec4, Vec4, Quat
-);
+impl_shader_layout!(4, 4, I16Vec2, U16Vec2);
 impl_shader_layout!(8, 6, I16Vec3, U16Vec3);
+impl_shader_layout!(8, 8, I16Vec4, U16Vec4);
+impl_shader_layout!(8, 8, IVec2, UVec2, Vec2);
 impl_shader_layout!(16, 12, IVec3, UVec3, Vec3);
+impl_shader_layout!(16, 16, IVec4, UVec4, Vec4, Quat);
 
 // Matrix
+impl_shader_layout!(8, 16, Mat2);
 // Can't use `Mat3` as its column vectors are not properly aligned.
-impl_shader_layout_raw!(Mat2, Mat3A, Mat4);
+impl_shader_layout!(16, 48, Mat3A);
+impl_shader_layout!(16, 64, Mat4);
 
 // Array
 macro_rules! impl_shader_layout_array {
